fe43ce51726bbafffb5af4b252325ea55c26e2ca
Danylo Kondratiev <knightpp@proton.me>
2023-09-22 21:51:07 +0300

    Drop revision (#8)

--- modupdate.nix
+++ modupdate.nix
@@ -1,39 +1,28 @@
 {
   buildGoModule,
   lib,
-  self,
-}: let
-  gitRev =
-    if (self ? rev)
-    then self.rev
-    else "dirty";
-in
-  buildGoModule {
+}:
+buildGoModule {
+  name = "modupdate";
+  src = builtins.path {
     name = "modupdate";
-    src = builtins.path {
-      name = "modupdate";
-      path = ./.;
-      filter = path: type:
-        builtins.elem (/. + path) [
-          ./go.mod
-          ./go.sum
-          ./main.go
-          ./version.go
-        ];
-    };
-    vendorSha256 = "sha256-XU4kLbEAPCL8mNk4omk2OIijKdiiAsJKBfoKkJJfHkI=";
+    path = ./.;
+    filter = path: type:
+      builtins.elem (/. + path) [
+        ./go.mod
+        ./go.sum
+        ./main.go
+        ./version.go
+      ];
+  };
+  vendorSha256 = "sha256-XU4kLbEAPCL8mNk4omk2OIijKdiiAsJKBfoKkJJfHkI=";
 
-    ldflags = [
-      "-s"
-      "-w"
-      "-X 'main.version=${self.shortRev or ""}'"
-      "-X 'main.vcsCommit=${gitRev}'"
-    ];
+  ldflags = ["-s" "-w"];
 
-    meta = with lib; {
-      description = "Tool to update direct dependencies in go.mod";
-      homepage = "https://github.com/knightpp/modupdate";
-      license = licenses.mit;
-      maintainers = with maintainers; [knightpp];
-    };
-  }
+  meta = with lib; {
+    description = "Tool to update direct dependencies in go.mod";
+    homepage = "https://github.com/knightpp/modupdate";
+    license = licenses.mit;
+    maintainers = with maintainers; [knightpp];
+  };
+}
