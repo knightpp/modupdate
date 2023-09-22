e1f374822d0426ac9c65de5c5eafdc3384a6977f
Danylo Kondratiev <knightpp@proton.me>
2023-09-22 21:42:28 +0300

    Trust flake and use only go sources (#7)

--- modupdate.nix
+++ modupdate.nix
@@ -1,13 +1,8 @@
 {
   buildGoModule,
   lib,
-  nix-gitignore,
   self,
 }: let
-  path = builtins.path {
-    name = "modupdate";
-    path = ./.;
-  };
   gitRev =
     if (self ? rev)
     then self.rev
@@ -15,7 +10,17 @@
 in
   buildGoModule {
     name = "modupdate";
-    src = nix-gitignore.gitignoreSource [] path;
+    src = builtins.path {
+      name = "modupdate";
+      path = ./.;
+      filter = path: type:
+        builtins.elem (/. + path) [
+          ./go.mod
+          ./go.sum
+          ./main.go
+          ./version.go
+        ];
+    };
     vendorSha256 = "sha256-XU4kLbEAPCL8mNk4omk2OIijKdiiAsJKBfoKkJJfHkI=";
 
     ldflags = [
